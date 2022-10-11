d4bd39487073edf086a4cf276bee46e1f7a30dbc
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-10-11 15:06:46 +0600

    Learning variables of GO Lang

---
+++ Basic GO/Variables.go
@@ -0,0 +1,17 @@
+package main
+import ("fmt")
+
+func main() {
+	// Variables are declared using the var keyword
+	var x int32 = 10;
+
+	// Variables can be declared without an initial variable name
+	var student = "Jhon";
+
+	// type is inferred
+	y := 10.5;
+
+	fmt.Println(x);
+	fmt.Println(student);
+	fmt.Println(y);
+}
